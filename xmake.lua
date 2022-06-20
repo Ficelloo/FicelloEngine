07827b0a40d7e5f40ee3582ff72d2c91f55a042a
Ficelloo <59775826+Ficelloo@users.noreply.github.com>
2022-06-20 22:07:38 +0200

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,17 +1,16 @@
-add_rules("mode.debug","mode.release") --ajout d'un mode de debug et de release
-add_rules("plugin.vsxmake.autoupdate") --regle d'update auto de la solution pour les flemmards qui devs sous VS (ingnoré sous linux)
+add_rules("mode.debug","mode.release") 
+add_rules("plugin.vsxmake.autoupdate") 
 
-add_requires("libsdl") --requiert la SDL2
+add_requires("libsdl")
 add_requires("sol2")
 add_requires("entt")
---add_requires("sol2") --demande lua et sol2
 
-target("ficello") --nouvelle cible de compilation
-    set_kind("binary") --on veut un executable
-    set_targetdir("bin") --les binaires sont automatiquement envoye dans le dossier bin (pas build car deja pris par vs...)
-    set_languages("cxx20") --activation des features du C++20 (attention ne compile pas si le compilo ne supporte pas C++ 20)
-    add_files("src/*.cpp") --inclus tous les .cpp dans le dossier src
-    add_headerfiles("src/*.hpp") --pareil avec les headers
-    add_packages("libsdl") --ajout des packages a notre target 
+target("FicelloEngine") 
+    set_kind("binary") 
+    set_targetdir("bin") -- binary located in /bin
+    set_languages("cxx20") -- c++
+    add_files("src/*.cpp") 
+    add_headerfiles("src/*.hpp")
+    add_packages("libsdl") 
     add_packages("sol2")
     add_packages("entt")
