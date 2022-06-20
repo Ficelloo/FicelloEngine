443a1ed300c5ec1b8be58343a4e2e2178a9b8bfa
Ficelloo <59775826+Ficelloo@users.noreply.github.com>
2022-06-20 22:11:32 +0200

    Revert "make a first engine Class"

---
+++ src/Forms.cpp
@@ -0,0 +1,8 @@
+#include <SDL.h>
+
+extern "C" {
+	#include "lua.h"
+	#include "lualib.h"
+	#include "lauxlib.h"
+}
+
---
+++ xmake.lua
@@ -0,0 +1,13 @@
+add_rules("mode.debug","mode.release")
+add_rules("plugin.vsxmake.autoupdate")
+
+add_requires("libsdl") -- require sdl2
+add_requires("sol2") -- require sol2
+
+target("FicelloEngine") 
+    set_kind("binary") 
+    set_targetdir("bin") 
+    set_languages("cxx20") -- c++ 20
+    add_files("src/*.cpp") 
+    add_packages("libsdl") 
+    add_packages("sol2")
